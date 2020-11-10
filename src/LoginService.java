e98be3021e2a32be6a84add61e98defea5ddfa18
Vida Duodu <vidash50@gmail.com>
2020-11-10 16:20:01 -0500

    We are enjoying this new task

---
+++ src/LoginService.java
@@ -0,0 +1,7 @@
+public class LoginService {
+
+    public static void main(String[] args) {
+
+        System.out.println("Bring the changes in");
+    }
+}
---
+++ src/LoginUtility.java
@@ -0,0 +1,8 @@
+public class LoginUtility {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("We are about closing");
+    }
+}
